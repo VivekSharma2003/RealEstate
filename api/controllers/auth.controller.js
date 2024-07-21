a505c4bcca2995ec6f6db57e2b775c859200912a
Vivek Sharma <viveksh1952003@gmail.com>
2024-07-22 00:33:50 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -55,6 +55,7 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
+            sameSite: 'None',
             secure: true,
             maxAge: age,
         }).status(200).json(userInfo);
